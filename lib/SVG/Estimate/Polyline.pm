9c4e6ed9e8d6c4caba487b8a42852d77e0cb2678
colin <colink@perldreamer.com>
2016-06-07 16:10:04 -0500

    Add Polyline class

---
+++ lib/SVG/Estimate/Polyline.pm
@@ -0,0 +1,33 @@
+package SVG::Estimate::Polyline;
+
+use Moo;
+use Math::Trig qw/pi/;
+
+extends 'SVG::Estimate::Shape';
+
+has points => (
+    is => 'ro',
+);
+
+sub draw_start {
+    my $self = shift;
+    ##Start drawing at the first point
+    return @{ $self->points->[0] };
+}
+
+sub shape_length {
+    my $self = shift;
+    my @points = @{ $self->points };
+    my $start = shift @points;
+    my $length = 0;
+    ##Iterate over the points and find the delta between them
+    foreach my $point (@points) {
+        my $dx = $point->[0] - $start->[0];
+        my $dy = $point->[1] - $start->[1];
+        $length += sqrt ($dx**2 + $dy**2);
+        $start = $point;
+    }
+    return $length;
+}
+
+1;
