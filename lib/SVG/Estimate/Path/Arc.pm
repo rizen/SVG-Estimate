f7cdf75c87be3f908ebdc16c76b7d019ac08b99c
colin <colink@perldreamer.com>
2016-06-10 21:07:04 -0500

    Fix arc length and center conversion, more tests

--- lib/SVG/Estimate/Path/Arc.pm
+++ lib/SVG/Estimate/Path/Arc.pm
@@ -80,9 +80,6 @@ sub BUILD {
     my $x1prim = $cosr * $dx + $sinr * $dy; #*
     my $y1prim = -1*$sinr * $dx + $cosr * $dy; #*
 
-    warn "x1prime: $x1prim\n";
-    warn "y1prime: $y1prim\n";
-
     my $x1prim_sq = $x1prim**2; #*
     my $y1prim_sq = $y1prim**2; #*
 
@@ -92,56 +89,36 @@ sub BUILD {
     my $rx_sq = $rx**2; #*
     my $ry_sq = $ry**2; #*
 
-    #my $radius_check = ($x1prim_sq / $rx_sq) + ($y1prim_sq / $ry_sq);
-    #if ($radius_check > 1) {
-    #    $rx *= sqrt($radius_check);
-    #    $ry *= sqrt($radius_check);
-    #    $rx_sq = $rx**2;
-    #    $ry_sq = $ry**2;
-    #}
-
     my $t1 = $rx_sq * $y1prim_sq;
     my $t2 = $ry_sq * $x1prim_sq;
     my $ts = $t1 + $t2;
     my $c  = sqrt(abs( (($rx_sq * $ry_sq) - $ts) / ($ts) ) );
-
-    warn "rx^2ry^2: ".($rx_sq * $ry_sq);
-    warn "ts: $ts\n";
-    warn "c: $c\n";
-    
-    if ($self->large_arc_flag == $self->sweep_flag) {
-        $c *= -1;
-    }
-    my $cxprim =     $c * $rx * $y1prim / $ry;
-    my $cyprim = -1 *$c * $ry * $x1prim / $rx;
 
     $self->_center([
         ($cosr * $cxprim - $sinr * $cyprim) + ( ($self->start_point->[0] + $self->point->[0]) / 2 ),
         ($sinr * $cxprim + $cosr * $cyprim) + ( ($self->start_point->[1] + $self->point->[1]) / 2 )
     ]);
 
-    my $ux = ($x1prim - $cxprim) / $rx;
-    my $uy = ($y1prim - $cyprim) / $ry;
-    my $vx = -1 * ($x1prim + $cxprim) / $rx;
-    my $vy = -1 * ($y1prim + $cyprim) / $ry;
+    ##**
+
+    ##Theta calculation
+    my $ux = ($x1prim - $cxprim) / $rx;   #*
+    my $uy = ($y1prim - $cyprim) / $ry;   #*
     my $n = sqrt($ux**2 + $uy**2);
     my $p = $ux;
+    my $d = $p / $n;
     my $theta = rad2deg(acos($p/$n));
     if ($uy < 0) {
         $theta *= -1;
     }
     $self->_theta($theta % 360);
 
+    my $vx = -1 * ($x1prim + $cxprim) / $rx;
+    my $vy = -1 * ($y1prim + $cyprim) / $ry;
     $n = sqrt( ($ux**2 + $uy**2) * ($vx**2 + $vy**2));
     $p = $ux*$vx + $uy*$vy;
-    my $d = $p / $n;
+    $d = $p / $n;
 
-    if ($d > 1) {
-        $d = 1;
-    }
-    elsif ($d < -1) {
-        $d = -1;
-    }
     my $delta = rad2deg(acos($d));
     if (($ux * $vy - $uy * $vx) < 0 ) {
         $delta *= -1;
--- t/Path/06_arc.t
+++ t/Path/06_arc.t
@@ -4,24 +4,27 @@ use lib 'lib', '../../lib', '../lib';
 
 use_ok 'SVG::Estimate::Path::Arc';
 my $arc = SVG::Estimate::Path::Arc->new(
-    start_point     => [7, 5],
-    rx              => 2,
-    ry              => 2,
+    start_point     => [275, 25],
+    rx              => 150,
+    ry              => 150,
     x_axis_rotation => 0,
     sweep_flag      => 0,
     large_arc_flag  => 0,
-    end             => [5, 7],
+    end             => [125, 175],
 );
 isa_ok $arc, 'SVG::Estimate::Path::Arc';
 
-is_deeply $arc->start_point, [7, 5], 'arc start point';
-is_deeply $arc->end_point, [5,7], 'arc end point';
-cmp_ok $arc->round($arc->length),  '==', 3.142, 'arc length';
+is_deeply $arc->start_point, [275, 25], 'arc start point';
+is_deeply $arc->end_point, [125,175], 'arc end point';
+is_deeply $arc->_center,   [275,175],  'calculated center point';
+cmp_ok $arc->round($arc->_theta), '==', 270, 'calculated theta (angle to initial point on x-axis)';
+cmp_ok $arc->round($arc->_delta), '==', -90, 'calculated delta (angle to initial point on x-axis)';
+cmp_ok $arc->round($arc->length),  '==', 235.618, 'arc length'; #( 2* pi * r / 4);
 
-cmp_ok $arc->round($arc->min_x), '==', 5, 'min_x';
-cmp_ok $arc->round($arc->max_x), '==', 7, 'max_x';
-cmp_ok $arc->round($arc->min_y), '==', 3, 'min_y';
-cmp_ok $arc->round($arc->max_y), '==', 5, 'max_y';
+cmp_ok $arc->round($arc->min_x), '==', 125, 'min_x';
+cmp_ok $arc->round($arc->max_x), '==', 275, 'max_x';
+cmp_ok $arc->round($arc->min_y), '==', 25, 'min_y';
+cmp_ok $arc->round($arc->max_y), '==', 175, 'max_y';
 
 done_testing();
 
