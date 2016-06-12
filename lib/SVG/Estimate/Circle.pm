afc79ca335484ec7ecfd1a238707766886da5dcf
JT Smith <jt@plainblack.com>
2016-06-11 21:01:54 -0500

    start fixing circle

--- lib/SVG/Estimate/Circle.pm
+++ lib/SVG/Estimate/Circle.pm
@@ -91,4 +91,27 @@ sub max_y {
     return $self->cy + $self->r;
 }
 
+sub BUILDARGS {
+    my ($class, @args) = @_;
+    ##Upgrade to hashref
+    my $args = @args % 2 ? $args[0] : { @args };
+    my $center   = [ $args->{cx}, $args->{cy} ];
+    my $opposite = [ $args->{x} + $args->{width}, $args->{y} + $args->{height} ];
+    if ($args->{transform}->has_transforms) {
+        $center   = $args->{transform}->transform($center);
+        $opposite = $args->{transform}->transform($opposite);
+        $args->{x} = $origin->[0] < $opposite->[0] ? $origin->[0] : $opposite->[0];
+        $args->{y} = $origin->[1] < $opposite->[1] ? $origin->[1] : $opposite->[1];
+        $args->{width}  = abs($opposite->[0] - $origin->[0]);
+        $args->{height} = abs($opposite->[1] - $origin->[1]);
+    }
+    $args->{draw_start}   = $center;
+    $args->{draw_end}     = $center;
+    $args->{shape_length} = ($args->{width} + $args->{height}) * 2;
+    $args->{min_x}        = $origin->[0];
+    $args->{min_y}        = $origin->[1];
+    $args->{max_x}        = $opposite->[0];
+    $args->{max_y}        = $opposite->[1];
+    return $args;
+}
 1;
